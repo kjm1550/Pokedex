e6b3aaec5e3883b2c1a1f3f54e6f492172fe3686
kjm1550 <kjm1550@gmail.com>
2020-08-26 21:54:09 -0500

    Updates

---
+++ precache-manifest.379ad8f49ab7f1c71e8c08adec11a7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848e0c7f349064e4a0f467d8d9926464",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "8299f49c8294663f2f26",
+    "url": "/Pokedex/static/css/main.6a595c0f.chunk.css"
+  },
+  {
+    "revision": "2448bc44a6e7b1af2127",
+    "url": "/Pokedex/static/js/2.818e2a09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.818e2a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8299f49c8294663f2f26",
+    "url": "/Pokedex/static/js/main.f6835b6a.chunk.js"
+  },
+  {
+    "revision": "9fb1140b615de1adf3f4",
+    "url": "/Pokedex/static/js/runtime-main.e9efafc6.js"
+  }
+]);
